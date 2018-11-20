51a6c336437ec783001b96287b52a3efebe01071
wangwujun <wangwujun@cmss.chinamobile.com>
2018-11-20 19:21:09 +0800

    menu build

---
+++ src/app/app-path.const.ts
@@ -0,0 +1,35 @@
+export const appPath = Object.freeze({
+
+  // 首页
+  home: '',
+
+  // 甜点
+  products: 'products',
+
+  // 登录
+  login: 'login',
+
+  // 购物车
+  cart: 'cart',
+
+  // 结账
+  checkout: 'checkout',
+
+  // 结账流程
+  checkoutFlow: {
+
+    // 推送
+    customerInfo: 'customer-info',
+
+    // 付款
+    paymentInfo: 'payment-info',
+
+    // 发票
+    receiptInfo: 'receipt-info'
+
+  },
+
+  // 结账成功
+  success: 'success'
+
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+// Constant
+import { appPath } from './app-path.const';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shop';
+  /**
+   * 給 Template 用的路由定義
+   *
+   * @ memberof AppComponent
+   */
+  path = appPath;
 }
